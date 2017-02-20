6779d55b72bcbb092a26f44ecb11fad406e90e68
wangwenhaihuling <1770492126@qq.com>
2017-02-20 10:44:20 +0800

    王文海  zhuanquApp提交

--- CustWebRoot/pageApp/OnlineserviceApp/js/controllers.js
+++ CustWebRoot/pageApp/OnlineserviceApp/js/controllers.js
@@ -146,13 +146,13 @@ angular.module("indexApp.controllers",[])
             var html="<ion-item style='border: none;min-height:100px;max-height:360px;padding: 0px;background-color: #EEEEEE;'>";
             html+="<div class='list' style='background-color: #EEEEEE;'>";
             html+="<div class='item' style='width: 100%; border: none;background-color: #EEEEEE;'>";
-            html+="<div style='min-height: 50px;padding: 2%;line-height:150%;max-width:33%;min-width:5%;float: right;background-image: url(img/dialog_box@2x.png);background-size: 100% 100%;background-repeat: no-repeat;white-space: normal;font-size:1em;'>";
+            html+="<div style='min-height: 50px;padding: 20px;line-height:150%;max-width:33%;min-width:5%;float: right;background-image: url(img/dialog_box@2x.png);background-size: 100% 100%;background-repeat: no-repeat;white-space: normal;font-size:1em;'>";
             html+=send;
             html+="</div>";
             html+="</div>";
             if(reply!=null) {
                 html += "<div class='item' style='width: 100%;border: none;background-color: #EEEEEE;'>";
-                html += "<div style='min-height: 50px;min-width: 5%;line-height:150%;max-width:50%;float: left;background-image: url(img/dialog_kf@2x.png);background-size: 100% 100%;background-repeat: no-repeat;white-space: normal;padding: 2%;font-size:1em;'>";
+                html += "<div style='min-height: 50px;min-width: 5%;line-height:150%;max-width:50%;float: left;background-image: url(img/dialog_kf@2x.png);background-size: 100% 100%;background-repeat: no-repeat;white-space: normal;padding: 20px;font-size:1em;'>";
                 html += reply;
                 html += "</div>";
                 html += "</div>";
